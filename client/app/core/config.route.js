39c809f0e2d1abc7d75b4284300a7b3848af3f2c
poyal <ttt4504@gmail.com>
2017-10-07 00:14:03 +0900

    404 Not Found 추가

--- client/app/core/config.route.js
+++ client/app/core/config.route.js
@@ -12,10 +12,14 @@
             templateUrl: 'app/menu/menu.html',
             controller: 'AppController',
             controllerAs: '$root'
+          })
+          .state('404', {
+            url: '/404',
+            templateUrl: 'app/templates/404.html'
           });
         $urlRouterProvider.when('', '/app/main');
         $urlRouterProvider.when('/', '/app/main');
-        $urlRouterProvider.otherwise('/app/main');
+        $urlRouterProvider.otherwise('/404');
       }
     ]);
 })();
