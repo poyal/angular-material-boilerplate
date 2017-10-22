c69acbb3aa4bbfe00dd995046bf886b5b5e0fcde
poyal <ttt4504@gmail.com>
2017-10-22 15:26:38 +0900

    테마 timeout 1초

--- client/app/core/theme/theme.config.js
+++ client/app/core/theme/theme.config.js
@@ -47,7 +47,7 @@
       if (!$rootScope.$pageFinishedLoading) {
         $rootScope.$pageFinishedLoading = true;
       }
-    }, 7000);
+    }, 1000);
 
     $rootScope.$baSidebarService = baSidebarService;
   }
